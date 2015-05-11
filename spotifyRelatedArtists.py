a5a6a625f87a087c97c33a4d77f2a9d2a51d68ad
James Thomson <james_thomson@ntlworld.com>
2015-05-11 20:58:56 +0100

    css style changes adn idoit proofing

--- spotifyRelatedArtists.py
+++ spotifyRelatedArtists.py
@@ -4,12 +4,40 @@ import webapp2
 import json
 from urllib import urlopen
 
-MAIN_PAGE_HTML = """\
+MAIN_PAGE_HTML = """
 <html>
     <style>
-    h1   {color:#ff7f00; font: sans-serif;}
-    h4   {font: sans-serif;}
-  </style>
+    body{border: 20px solid #E5E5E5; margin: 0; padding: 0;}
+    
+    h1 {padding-left: 2cm; color:#ff7f00; font: 40px sans-serif; font-weight: bold}
+    h4 {padding-left: 2cm;  font: 15px sans-serif; font-weight: bold; }
+    p, div{padding-left: 2cm; font: 15px sans-serif}
+    label {padding-left: 0.2cm; font: 15px sans-serif}
+    p2 {padding-left: 4cm; 15px sans-serif} 
+    
+    a:link {color: #ff7f00;}
+    a:visited {color: #ff7f00;}
+    a:hover { color: #FFBF7F;}
+    
+    .text-input{font: 15px sans-serif;}
+    
+    .btn-style{
+    border : solid 5px #ff7f00;
+    border-radius : 3px;
+    moz-border-radius : 3px;
+    -webkit-box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    -moz-box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    font-size : 15px;
+    color : #000000;
+    font-weight: bold;
+    padding : 3px 14px;
+    background-color : #F4F4F4;}
+    
+
+    
+    </style>
+  
   <body>
     <form action="/sign" method="post">
     <br>
@@ -17,19 +45,19 @@ MAIN_PAGE_HTML = """\
     <h4>This app generates a visualisation of any Spotify artists by calling the spotify API and putting the data into a d3.js style visualisation
     <br>One stage gathers all the related artists to the requested artists
     <br>Two stage gathers all the related artists to those gathered at the first stage
-    </h4>
-    <h4>For more details please visit <a href="http://myinspirationinformation.com/" target="_blank">Inspiration Information</a></h4>
-    <br>Choose how many stages of related artists would you like: <br><br>
-    <INPUT TYPE="Radio" Name="stages" Value="1">1-Stage
-    <INPUT TYPE="Radio" Name="stages" Value="2">2-Stage
+    <br><br>For more details please visit <a href="http://myinspirationinformation.com/visualisation/d3-js/spotify-related-artists-app/" target="_blank">Inspiration Information</a></h4>
+    <HR WIDTH=89% size=20 noshade color=#F4F4F4>
+    <p>Choose how many stages of related artists would you like: <br><br></p>
+    <p2><INPUT TYPE="Radio" Name="stages" Value="1" checked="checked"><label>1-Stage</label>
+    <INPUT TYPE="Radio" Name="stages" Value="2"><label>2-Stage</label>
     <br>
-    <br> If you request 2 stage please be patient it may take a minute
     <br>
-    <br> Enter the artist/band name(s). Use semi colons ; to seperate mulitple artists/bands: 
+    <p>If you request 2 stage please be patient it may take a minute
     <br>
-    <br><div><textarea name="band" rows="1" cols="60"></textarea></div>
-    <br><div><input type="submit" value="Make me my visualisation"></div>
-     
+    <br> Enter the artist/band name(s). Use semi colons ; to separate multiple artists/bands: 
+    </p>
+    <div><input class="text-input" type="text" placeholder=" Enter band name" name="band" size=60 ></div>
+    <br><div><input type="submit" class="btn-style" value="Make me my visualisation"></div>
     </form>
   </body>
 </html>
@@ -57,91 +85,108 @@ class Guestbook(webapp2.RequestHandler):
           art_url='https://api.spotify.com/v1/search?q='+artist+'&type=artist'
           urlout=urlopen(art_url).read()
           result=json.loads(urlout)
-          input_artist_id.append(result['artists']['items'][0]['id'])
+          if result['artists']['items']:
+            input_artist_id.append(result['artists']['items'][0]['id'])
         
-        #iterate through each id find related artists
-        artist_id=[]
-        for id in input_artist_id:
-          if id not in artist_id:
-            artist_id.append(id)
-            art_url='https://api.spotify.com/v1/artists/'+id
-            urlout=urlopen(art_url).read()
-            result=json.loads(urlout)
-            nodes={'name':result['name'],'group':0,'popularity':result['popularity']}
-            json_nodes = json_nodes + json.dumps(nodes) +','
-          rel_url='https://api.spotify.com/v1/artists/'+id+'/related-artists'
-          urlout=urlopen(rel_url).read()
-          related=json.loads(urlout)
-          
-          #iterate through each related artist and add to the nodes and links   
-          for r in range(20):
-            if related['artists'][r]['uri'].replace("spotify:artist:","") not in artist_id:
-              artist_id.append(related['artists'][r]['uri'].replace("spotify:artist:",""))
-              if related['artists'][r]['uri'].replace("spotify:artist:","") in input_artist_id:
-                nodes={'name':related['artists'][r]['name'],'group':0,'popularity':related['artists'][r]['popularity']}
-              else:  
-                nodes={'name':related['artists'][r]['name'],'group':1,'popularity':related['artists'][r]['popularity']}
-              json_nodes = json_nodes + json.dumps(nodes) + ','
-            edges={'source':artist_id.index(id),'target':artist_id.index(related['artists'][r]['uri'].replace("spotify:artist:","")), 
-                   'value':1, 'distance':40+r*5}
-            json_links = json_links + json.dumps(edges) + ','
-        
-        #if 2 stage is requested iterate through each related artist and find further related artists
-        if stages=='2':
-          artist_id2=list(set(artist_id)-set(input_artist_id))  
-          for id in artist_id2:
-            rel_url='https://api.spotify.com/v1/artists/'+id+'/related-artists'
-            urlout=urlopen(rel_url).read()
-            related=json.loads(urlout)
+        if len(inputs)==len(input_artist_id):
+            #iterate through each id find related artists
+            artist_id=[]
+            for id in input_artist_id:
+              if id not in artist_id:
+                artist_id.append(id)
+                art_url='https://api.spotify.com/v1/artists/'+id
+                urlout=urlopen(art_url).read()
+                result=json.loads(urlout)
+                nodes={'name':result['name'],'group':0,'popularity':result['popularity']}
+                json_nodes = json_nodes + json.dumps(nodes) +','
+              rel_url='https://api.spotify.com/v1/artists/'+id+'/related-artists'
+              urlout=urlopen(rel_url).read()
+              related=json.loads(urlout)
               
-            for r in range(20):
-              if related['artists'][r]['uri'].replace("spotify:artist:","") not in artist_id:
-                artist_id.append(related['artists'][r]['uri'].replace("spotify:artist:",""))
-                nodes={'name':related['artists'][r]['name'],'group':2,'popularity':related['artists'][r]['popularity']}
-                json_nodes = json_nodes + json.dumps(nodes) + ','
-              edges={'source':artist_id.index(id),'target':artist_id.index(related['artists'][r]['uri'].replace("spotify:artist:","")), 
-                     'value':1, 'distance':40+r*5}
-              json_links = json_links + json.dumps(edges) + ','
-              
-              
-        json_nodes = json_nodes[:-1]
-        json_links = json_links[:-1]
-        json_links = json_links + "]}"
-        
-        json_final = json_nodes + json_links
+              #iterate through each related artist and add to the nodes and links   
+              for r in range(20):
+                if related['artists'][r]['uri'].replace("spotify:artist:","") not in artist_id:
+                  artist_id.append(related['artists'][r]['uri'].replace("spotify:artist:",""))
+                  if related['artists'][r]['uri'].replace("spotify:artist:","") in input_artist_id:
+                    nodes={'name':related['artists'][r]['name'],'group':0,'popularity':related['artists'][r]['popularity']}
+                  else:  
+                    nodes={'name':related['artists'][r]['name'],'group':1,'popularity':related['artists'][r]['popularity']}
+                  json_nodes = json_nodes + json.dumps(nodes) + ','
+                edges={'source':artist_id.index(id),'target':artist_id.index(related['artists'][r]['uri'].replace("spotify:artist:","")), 
+                       'value':1, 'distance':40+r*5}
+                json_links = json_links + json.dumps(edges) + ','
+            
+            #if 2 stage is requested iterate through each related artist and find further related artists
+            if stages=='2':
+              artist_id2=list(set(artist_id)-set(input_artist_id))  
+              for id in artist_id2:
+                rel_url='https://api.spotify.com/v1/artists/'+id+'/related-artists'
+                urlout=urlopen(rel_url).read()
+                related=json.loads(urlout)
+                  
+                for r in range(20):
+                  if related['artists'][r]['uri'].replace("spotify:artist:","") not in artist_id:
+                    artist_id.append(related['artists'][r]['uri'].replace("spotify:artist:",""))
+                    nodes={'name':related['artists'][r]['name'],'group':2,'popularity':related['artists'][r]['popularity']}
+                    json_nodes = json_nodes + json.dumps(nodes) + ','
+                  edges={'source':artist_id.index(id),'target':artist_id.index(related['artists'][r]['uri'].replace("spotify:artist:","")), 
+                         'value':1, 'distance':40+r*5}
+                  json_links = json_links + json.dumps(edges) + ','
+                  
+                  
+            json_nodes = json_nodes[:-1]
+            json_links = json_links[:-1]
+            json_links = json_links + "]}"
+            
+            json_final = json_nodes + json_links
 
         outputstart = """<!DOCTYPE html>
     
     <style>
+    body{border: 20px solid #E5E5E5; margin: 0; padding: 0;}
     
-    .link {
-    stroke: #ccc;
-    }
+    .link {stroke: #ccc;}
     
-    .node text {
-    pointer-events: none;
-    font: 10px sans-serif;
-    }
+    .node text {pointer-events: none;font: 10px sans-serif;    }
     
-    h1   {color:#ff7f00; font: sans-serif;}
-    h4   {font: sans-serif;}
+    h1 {padding-left: 2cm; color:#ff7f00; font: 40px sans-serif; font-weight: bold}
+    h4 {padding-left: 2cm; font: 15px sans-serif; font-weight: bold}
+    
+    a:link {color: #ff7f00;}
+    a:visited {color: #ff7f00;}
+    a:hover { color: #FFBF7F;}
+    
+    .form-style{    
+    border : solid 5px #ff7f00;
+    border-radius : 3px;
+    moz-border-radius : 3px;
+    -webkit-box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    -moz-box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    box-shadow : 0px 0px 5px rgba(0,0,0,1.0);
+    font-size : 15px;
+    color : #000000;
+    font-weight: bold;
+    padding : 3px 14px;
+    background-color : #F4F4F4;}
     
     </style>
     <body>
     <br>
     <h1>SPOTIFY RELATED ARTISTS VISUALISATION<h1/>
-    <h4>Built by:
-    <br><a href="http://myinspirationinformation.com/" target="_blank">Inspiration Information</a>
-    <br>Thanks to:
-    <br><a href="https://developer.spotify.com/web-api/" target="_blank">Spotify API</a>
-    <br><a href="https://github.com/mbostock/d3/wiki/Gallery" target="_blank">Mike Bostock's D3 Project</a>
+    <h4>Built by: <a href="http://myinspirationinformation.com/" target="_blank">Inspiration Information</a>
+    <br><br>Thanks to:<a href="https://developer.spotify.com/web-api/" target="_blank">Spotify API</a> and 
+    <a href="https://github.com/mbostock/d3/wiki/Gallery" target="_blank">Mike Bostock's D3 Project</a></h4>
+    <br><HR WIDTH=89% size=20 noshade color=#F4F4F4>
+    <h4><form action="http://spotify-related-artists-1.appspot.com/">
+    <input type="submit" class="form-style" value="Make Another">
+    </form>
     </h4>
 
-    <script src=\"http://d3js.org/d3.v3.min.js\"></script>
+    <script src="http://d3js.org/d3.v3.min.js"></script>
     
-    <script type='text/javascript' src=\"http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js\"> </script>
+    <script type='text/javascript' src="http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"> </script>
     
-    <script type=\"application/json\" id=\"mis\">
+    <script type="application/json" id="mis">
     """
         
         outputend = """</script>
@@ -242,9 +287,18 @@ class Guestbook(webapp2.RequestHandler):
  </html>"""
 
 
-        self.response.write(outputstart)
-        self.response.write(json_final)
-        self.response.write(outputend)
+
+
+        if len(inputs)==len(input_artist_id):
+            self.response.write(outputstart)
+            self.response.write(json_final)
+            self.response.write(outputend)
+        else:
+            self.response.write(outputstart)
+            self.response.write('</script><h4><br>You entered:<br><br>"')
+            self.response.write(band)            
+            self.response.write('"<br><br>One or more of your artists cant be found. Please try again</h4></body></html>')
+           
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
